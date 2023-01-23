41a3d9e849ab00b68d68e235fe75f97bda14bf89
madBit <vrushankraval48617@gmail.com>
2023-01-23 23:37:40 +0530

    The first cpp file and py file

---
+++ 2ndLargest.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+using namespace std;
+
+int secLarge(int arr[], int n) {
+
+    int a = 0;
+    int b = 0;
+
+    for(int i = 0; i<n; i++) {
+        if(arr[i]>a) 
+            a = arr[i];
+    }
+
+    for(int i = 0; i<n; i++) {
+        if(arr[i]>b && arr[i]<a) {
+            b = arr[i];
+            return b;
+        }
+    }
+
+    return -1;
+}
+
+int main() {
+
+    int n;
+    cin>>n;
+
+    int arr[1000];
+    for(int i = 0; i<n; i++) {
+        cin>>arr[i];
+    }
+
+    int ans = secLarge(arr,n);
+
+    cout<<ans<<endl;
+    return 0;
+}
